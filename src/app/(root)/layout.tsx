d46c9a2b6a17adcfb8672067a5d1d015e6ba9d6e
selly361 <seikou615@gmail.com>
2024-06-18 17:49:29 +0100

    Wrap children with `SessionProvider`

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,13 +1,16 @@
 import { League_Spartan } from 'next/font/google'
 import '../globals.css'
 import { PropsWithChildren } from 'react'
+import { SessionProvider } from '@/Components'
 
 const leagueSpartan = League_Spartan({ subsets: ['latin'] })
 
 export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
 	return (
-		<html lang='en' className='theme-auto'>
-			<body className={leagueSpartan.className}>{children}</body>
-		</html>
+		<SessionProvider>
+			<html lang='en'>
+				<body className={leagueSpartan.className}>{children}</body>
+			</html>
+		</SessionProvider>
 	)
 }
