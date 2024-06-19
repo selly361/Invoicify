391492f0b04241bfc4531846790be6b53bac300f
selly361 <seikou615@gmail.com>
2024-06-19 14:04:55 +0100

    Update: add image remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,19 @@ const nextConfig = {
 			use: ['@svgr/webpack']
 		})
 		return config
+	},
+
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'avatars.githubusercontent.com'
+			},
+			{
+				protocol: 'https',
+				hostname: 'lh3.googleusercontent.com'
+			}
+		]
 	}
 }
 
