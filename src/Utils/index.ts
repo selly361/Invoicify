5b467b781af8072dc9542d18375b25a7616f6d00
selly361 <seikou615@gmail.com>
2024-06-24 09:51:39 +0100

    Update: add `generateCustomID` util function

--- src/Utils/index.ts
+++ src/Utils/index.ts
@@ -1,7 +1,18 @@
+import ShortUniqueId from 'short-unique-id'
+
 export const addDays = (date: Date, days: number) => {
 	const result = new Date(date)
 	result.setDate(result.getDate() + days)
 	return result
 }
 
+export const generateCustomId = () => {
+	const uidLetters = new ShortUniqueId({dictionary: 'abcdefghijklmnopqrstuvwxyz'.split('')})
+	const letters = uidLetters.randomUUID(3)
+
+	const uidNumbers = new ShortUniqueId({ dictionary: '0123456789'.split('') })
+	const numbers = uidNumbers.randomUUID(3)
+
+	return `${letters}${numbers}`
+}
 
