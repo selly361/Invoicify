20733d564cdba2cabe07025b6c21d892ae678234
selly361 <seikou615@gmail.com>
2024-06-26 14:20:49 +0100

    Update: add more util functions

--- src/Utils/index.ts
+++ src/Utils/index.ts
@@ -1,4 +1,7 @@
 import ShortUniqueId from 'short-unique-id'
+import { Item } from '@/Types'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 
 export const addDays = (date: Date, days: number) => {
 	const result = new Date(date)
@@ -7,12 +10,37 @@ export const addDays = (date: Date, days: number) => {
 }
 
 export const generateCustomId = () => {
-	const uidLetters = new ShortUniqueId({dictionary: 'abcdefghijklmnopqrstuvwxyz'.split('')})
-	const letters = uidLetters.randomUUID(3)
+	const uidLetters = new ShortUniqueId({
+		dictionary: 'abcdefghijklmnopqrstuvwxyz'.split('')
+	})
+	const letters = uidLetters.randomUUID(2)
 
 	const uidNumbers = new ShortUniqueId({ dictionary: '0123456789'.split('') })
-	const numbers = uidNumbers.randomUUID(3)
+	const numbers = uidNumbers.randomUUID(4)
 
-	return `${letters}${numbers}`
+	return `${letters}${numbers}`.toUpperCase()
 }
 
+export const formatDate = (date: Date) => {
+	return date?.toISOString().split('T')[0]
+}
+
+export const totalPriceOfItems = (items: Item[]) => {
+	const total = items.reduce((a, b) => (a += b.total), 0)
+	return total.toLocaleString()
+}
+
+
+export const getSessionUserId = async () => {
+	const session = await getServerSession(authOptions)
+	if (!session?.user?.id) {
+		console.error('Session or user ID not found')
+		throw new Error('Not authenticated')
+	}
+	return session.user.id
+}
+
+export const handleDBErrors = (error: any, message: string) => {
+	console.error(message, error)
+	throw new Error(message)
+}
