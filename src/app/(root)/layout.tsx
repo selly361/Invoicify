3ae5acbfae7391b2255a8e71e077eabb455d924c
selly361 <seikou615@gmail.com>
2024-06-27 12:19:04 +0100

    Update: refactor layout with providers, and components

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,16 +1,28 @@
 import { League_Spartan } from 'next/font/google'
 import '../globals.css'
 import { PropsWithChildren } from 'react'
-import { SessionProvider } from '@/Components'
+import { HandleModals, Header, Overlay, SessionProvider } from '@/Components'
+import { FormProvider, ModalContextProvider } from '@/Contexts'
 
 const leagueSpartan = League_Spartan({ subsets: ['latin'] })
 
 export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
 	return (
 		<SessionProvider>
-			<html lang='en'>
-				<body className={leagueSpartan.className}>{children}</body>
-			</html>
+			<ModalContextProvider>
+				<FormProvider>
+					<html lang='en' className='dark'>
+						<body
+							className={`${leagueSpartan.className} bg-bgMain min-h-screen flex justify-center pt-16`}
+						>
+							<Header />
+							<Overlay />
+							<HandleModals />
+							{children}
+						</body>
+					</html>
+				</FormProvider>
+			</ModalContextProvider>
 		</SessionProvider>
 	)
 }
