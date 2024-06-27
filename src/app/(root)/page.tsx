5dfa5738e20b415b0d5cf20abdd3e689b2073660
selly361 <seikou615@gmail.com>
2024-06-27 12:19:19 +0100

    Update: refactor home page

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,20 @@
-function Home() {
+import { InvoiceHeader, Invoices, NoInvoice } from "@/Components"
+import { fetchUserInvoices } from "@/Lib/Actions"
+
+async function Home() {
+
+  const invoicesData = await fetchUserInvoices()
+
+  const length = invoicesData?.length ?? 0
+
   return (
-    <main className='h-screen w-screen bg-bgMain'>
-      
-    </main>
+    <main className='w-[730px] min-h-24 bg-bgMain flex flex-col gap-16 pb-'>
+    <InvoiceHeader numberOfInvoices={length} />
+
+    <div className='w-full min-h-80 flex items-center justify-center'>
+      {length === 0 ? <NoInvoice /> : <Invoices invoicesData={invoicesData} />}
+    </div>
+  </main>
   )
 }
 
