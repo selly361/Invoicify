0729849c5d43f30c909aebaaac98083e7398a929
selly361 <seikou615@gmail.com>
2024-07-02 12:25:45 +0100

    Update: handle login modal

--- src/Components/Shared/HandleModals.tsx
+++ src/Components/Shared/HandleModals.tsx
@@ -2,16 +2,16 @@
 
 import { useModalContext } from '@/Contexts'
 import { AnimatePresence } from 'framer-motion'
-import { Form } from '@/Components'
+import { Form, DeleteModal, AuthModal } from '@/Components'
 
 function HandleModals() {
 	const { activeModal } = useModalContext()
 
 	return (
 		<AnimatePresence mode='wait'>
-			{activeModal === 'addInvoice' || activeModal === 'editInvoice' ? (
-				<Form />
-			) : null}
+			{activeModal === 'addInvoice' || activeModal === 'editInvoice' ? ( <Form />) : null}
+			{activeModal === 'delete' ? <DeleteModal /> : null}
+			{activeModal === 'login' ? <AuthModal /> : null}
 		</AnimatePresence>
 	)
 }
