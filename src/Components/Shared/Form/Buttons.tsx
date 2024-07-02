8d5f7250ebe8c82ad57df8eba309ab58f11c84fd
selly361 <seikou615@gmail.com>
2024-07-02 12:28:37 +0100

    Update: handle create draft button

--- src/Components/Shared/Form/Buttons.tsx
+++ src/Components/Shared/Form/Buttons.tsx
@@ -1,10 +1,18 @@
 'use client'
 
-import { useModalContext } from '@/Contexts'
+import { useFormContext, useModalContext } from '@/Contexts'
+import { createInvoiceDraft } from '@/Lib/Actions'
+import { Invoice } from '@/Types'
 
 function Buttons() {
 	const { closeModal, activeModal } = useModalContext()
+	const { getValues } = useFormContext()
 
+	const createDraft = async () => {
+        const data = getValues() as Invoice
+        await createInvoiceDraft(data)
+		closeModal()
+    }
 
 	return (
 		<div className='w-full h-12'>
@@ -13,7 +21,7 @@ function Buttons() {
 					<button
 						onClick={closeModal}
 						type='button'
-						className='button bg-[#F9FAFE] text-textTertiary'
+						className='button bg-[#F9FAFE] text-blueGrayish'
 					>
 						Discard
 					</button>
@@ -21,7 +29,7 @@ function Buttons() {
 
 				<div className='flex gap-2 h-28 items-center'>
 					{activeModal === 'addInvoice' && (
-						<button type='button' className='button-tertiary'>
+						<button onClick={createDraft} type='button' className='button-tertiary'>
 							Save as Draft
 						</button>
 					)}
