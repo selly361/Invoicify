2c325986d6a31804eb7d375dfaa23928b8ebc9d4
selly361 <seikou615@gmail.com>
2024-07-02 12:25:01 +0100

    Update: fix type error with this

--- src/Lib/Models/Invoice.ts
+++ src/Lib/Models/Invoice.ts
@@ -2,107 +2,91 @@ import { Item } from '@/Types'
 import { Schema, model, models } from 'mongoose'
 
 const AddressSchema = new Schema({
-  street: {
-    type: String,
-    required: function () { return this.status !== 'draft' }
-  },
-  city: {
-    type: String,
-    required: function () { return this.status !== 'draft' }
-  },
-  postCode: {
-    type: String,
-    required: function () { return this.status !== 'draft' }
-  },
-  country: {
-    type: String,
-    required: function () { return this.status !== 'draft' }
-  }
+	street: {
+		type: String
+	},
+	city: {
+		type: String
+	},
+	postCode: {
+		type: String
+	},
+	country: {
+		type: String
+	}
 })
 
 const ItemSchema = new Schema({
-  name: {
-    type: String,
-    required: function () { return this.status !== 'draft' }
-  },
-  quantity: {
-    type: Number,
-    required: function () { return this.status !== 'draft' },
-    min: 1
-  },
-  price: {
-    type: Number,
-    required: function () { return this.status !== 'draft' },
-    min: 0
-  },
-  total: {
-    type: Number,
-    required: function () { return this.status !== 'draft' },
-    min: 0
-  }
+	name: {
+		type: String
+	},
+	quantity: {
+		type: Number,
+		min: 1
+	},
+	price: {
+		type: Number,
+		min: 0
+	},
+	total: {
+		type: Number,
+		min: 0
+	}
 })
 
 const InvoiceSchema = new Schema({
-  invoiceId: {
-    type: String,
-    unique: true,
-    required: true
-  },
-  senderAddress: {
-    type: AddressSchema,
-    required: function () { return this.status !== 'draft' }
-  },
-  clientName: {
-    type: String,
-    required: function () { return this.status !== 'draft' }
-  },
-  clientEmail: {
-    type: String,
-    required: function () { return this.status !== 'draft' },
-    match: /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
-  },
-  clientAddress: {
-    type: AddressSchema,
-    required: function () { return this.status !== 'draft' }
-  },
-  createdAt: {
-    type: Date,
-    required: true,
-    default: Date.now
-  },
-  paymentDue: {
-    type: Date,
-    required: function () { return this.status !== 'draft' },
-    default: Date.now
-  },
-  paymentTerms: {
-    value: {
-      type: String,
-      required: function () { return this.status !== 'draft' },
-      enum: ['1', '7', '14', '30']
-    },
-    label: {
-      type: String,
-      required: function () { return this.status !== 'draft' }
-    }
-  },
-  status: {
-    type: String,
-    required: true,
-    default: 'draft'
-  },
-  description: {
-    type: String,
-    required: function () { return this.status !== 'draft' }
-  },
-  items: {
-    type: [ItemSchema],
-    validate: [arrayLimit, 'At least one item is required']
-  }
+	invoiceId: {
+		type: String,
+		unique: true,
+		required: true
+	},
+	senderAddress: {
+		type: AddressSchema,
+	},
+	clientName: {
+		type: String
+	},
+	clientEmail: {
+		type: String,
+		match:
+			/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+	},
+	clientAddress: {
+		type: AddressSchema
+	},
+	createdAt: {
+		type: Date,
+		required: true,
+		default: Date.now
+	},
+	paymentDue: {
+		type: Date,
+		required: true,
+		default: Date.now
+	},
+	paymentTerms: {
+		value: {
+			type: String,
+			required: true,
+			enum: ['1', '7', '14', '30']
+		},
+		label: {
+			type: String,
+			required: true
+		}
+	},
+	status: {
+		type: String,
+		required: true,
+		default: 'draft'
+	},
+	description: {
+		type: String,
+		required: false
+	},
+	items: {
+		type: [ItemSchema]
+	}
 })
-
-function arrayLimit(val: Item[]) {
-  return this.status === 'draft' || val.length > 0;
-}
 
 export default models.Invoice || model('Invoice', InvoiceSchema)
