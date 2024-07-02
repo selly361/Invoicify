cb5a2d04cddfe7d1d5443d47dcc9e9c489e0c766
selly361 <seikou615@gmail.com>
2024-07-02 12:35:34 +0100

    Update: refactor with markup and with invoice data

--- src/app/(root)/invoice/[invoiceId]/page.tsx
+++ src/app/(root)/invoice/[invoiceId]/page.tsx
@@ -1,13 +1,29 @@
+import { InvoiceButtonsHeader, InvoiceInfo } from "@/Components"
+import { IconArrowLeft } from "@/Icons"
+import { fetchInvoice } from "@/Lib/Actions"
+import Link from "next/link"
+
 interface Props {
     params: {
         invoiceId: string
     }
 }
 
-function page({ params }: Props) {
+async function page({ params }: Props) {
+
+  const invoice = await fetchInvoice(params.invoiceId)
+
+  if(!invoice) return
+
   return (
-    <div>
-        <h1 className='text-textPrimary text-heading-l'>{params.invoiceId}</h1>
+    <div className='w-full h-max flex flex-col mb-12'>
+        <Link href='/' className='hover:text-grayMedium text-textPrimary mb-8 flex items-center gap-6 w-max'>
+          <IconArrowLeft />
+          <p className="text-heading-s transition-color duration-300">Go back</p>
+        </Link>
+
+      <InvoiceButtonsHeader status={invoice.status} />
+      <InvoiceInfo invoice={invoice} />
     </div>
   )
 }
